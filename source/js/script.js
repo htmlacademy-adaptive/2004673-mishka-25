29c8828751811f1ffc60f1a33dbbdd102648db01
Vita <k.raynes145@gmail.com>
2022-05-24 20:50:33 +1000

    Внесла предзащитные правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,19 @@
 let navMain = document.querySelector('.navigation__menu');
-let navToggle = document.querySelector('.menu__button');
-let closeIcon = document.querySelector('.menu__icon-close');
-let menuIcon = document.querySelector('.menu__icon-open');
-let ItemList = document.querySelector('.menu__wrapper');
+let navToggle = document.querySelector('.navigation__button');
+let closeIcon = document.querySelector('.navigation__icon-close');
+let menuIcon = document.querySelector('.navigation__icon-open');
+let ItemList = document.querySelector('.navigation__wrapper');
 
-navMain.classList.remove('menu__nojs');
+navMain.classList.remove('navigation__menu-nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('menu__close')) {
-    navMain.classList.replace('menu__close', 'menu__open');
+  if (navMain.classList.contains('navigation__menu-close')) {
+    navMain.classList.replace('navigation__menu-close', 'navigation__menu-open');
     closeIcon.style.display = 'block';
     menuIcon.style.display = 'none';
     ItemList.style.display = 'flex';
   } else {
-    navMain.classList.replace('menu__open', 'menu__close');
+    navMain.classList.replace('navigation__menu-open', 'navigation__menu-close');
     closeIcon.style.display = 'none';
     menuIcon.style.display = 'block';
     ItemList.style.display = 'none';
